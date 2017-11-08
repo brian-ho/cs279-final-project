865f355a5927a5653bd64c32914f0fecfb515ddc
Brian Ho <ho_brian@me.com>
2017-11-08 16:42:26 -0500

    Update access

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from boto.mturk.price import Price
 
 #Start Configuration Variables
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 DEV_ENVIROMENT_BOOLEAN = True
 DEBUG = True
 #End Configuration Variables
