ee5368117ad21808b0a774b507eabb1afaac770d
Brian Ho <ho_brian@me.com>
2017-11-08 11:44:30 -0500

    Update name with template

--- main.py
+++ main.py
@@ -45,7 +45,11 @@ def verify():
         "assignment_id": request.args.get("assignmentId"),
         "amazon_host": AMAZON_HOST,
         "hit_id": request.args.get("hitId"),
-        "some_info_to_pass": request.args.get("someInfoToPass")
+        "some_info_to_pass": request.args.get("someInfoToPass"),
+        "img1": : "img1",
+        "img2": : "img2",
+        "img3": : "img3",
+        "img4": : "img4"
     }
 
     resp = make_response(render_template("verify.html", name = render_data))
