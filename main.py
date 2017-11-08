d377502277fb5a83870d2bf0f9c067f9334500a0
Brian Ho <ho_brian@me.com>
2017-11-08 15:14:01 -0500

    Update gmaps

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ DEV_ENVIROMENT_BOOLEAN = True
 DEBUG = True
 #End Configuration Variables
 
+GMAPS_KEY = "os.environ['GMAPS_KEY']"
+
 #This allows us to specify whether we are pushing to the sandbox or live site.
 if DEV_ENVIROMENT_BOOLEAN:
     AMAZON_HOST = "https://workersandbox.mturk.com/mturk/externalSubmit"
@@ -24,6 +26,37 @@ app = Flask(__name__, static_url_path='')
 @app.route('/')
 def main():
     return render_template('home.html')
+
+@app.route('/find')#, methods=['GET', 'POST'])
+def find():
+#The following code segment can be used to check if the turker has accepted the task yet
+    if request.args.get("assignmentId") == "ASSIGNMENT_ID_NOT_AVAILABLE":
+        #Our worker hasn't accepted the HIT (task) yet
+        pass
+    else:
+        #Our worker accepted the task
+        pass
+
+    '''
+    We're creating a dict with which we'll render our template page.html
+    Note we are grabbing GET Parameters
+    In this case, I'm using someInfoToPass as a sample parameter to pass information
+    '''
+    render_data = {
+        "worker_id": request.args.get("workerId"),
+        "assignment_id": request.args.get("assignmentId"),
+        "amazon_host": AMAZON_HOST,
+        "hit_id": request.args.get("hitId"),
+        "some_info_to_pass": request.args.get("someInfoToPass")
+        "some_info_to_pass": request.args.get("someInfoToPass"),
+        "gmaps_url": "https://maps.googleapis.com/maps/api/js?key=Y"+GMAPS_KEY+"&callback=initialize"
+        }
+
+    resp = make_response(render_template("find.html", name = render_data))
+    #This is particularly nasty gotcha.
+    #Without this header, your iFrame will not render in Amazon
+    resp.headers['x-frame-options'] = 'this_can_be_anything'
+    return resp
 
 @app.route('/verify')#, methods=['GET', 'POST'])
 def verify():
