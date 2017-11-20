0a7548551426c09dd2b9a415d310ddccdc8d189c
Brian Ho <ho_brian@me.com>
2017-11-20 00:44:29 -0500

    Testing PostGres

--- AWS_monitor.py
+++ AWS_monitor.py
@@ -1,6 +1,5 @@
 import os
-import sys
-#import setup
+# import setup
 from flask import Flask, render_template, url_for, request, make_response
 from boto.mturk.connection import MTurkConnection
 from boto.mturk.question import ExternalQuestion
@@ -8,13 +7,11 @@ from boto.mturk.qualification import Qualifications, PercentAssignmentsApprovedR
 from boto.mturk.price import Price
 
 #Start Configuration Variables
-AWS_ACCESS_KEY_ID = "AKIAIRJSQWZHVQIIHXCQ"
-AWS_SECRET_ACCESS_KEY = "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"
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 DEV_ENVIROMENT_BOOLEAN = True
 DEBUG = True
 #End Configuration Variables
-
-AWS_id = sys.argv[1]
 
 #This allows us to specify whether we are pushing to the sandbox or live site.
 if DEV_ENVIROMENT_BOOLEAN:
@@ -25,9 +22,3 @@ else:
 connection = MTurkConnection(aws_access_key_id=AWS_ACCESS_KEY_ID,
                              aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
                              host=AMAZON_HOST)
-
-results = connection.get_assignments(AWS_id)
-assignment = results[0]
-# print results[0].WorkerId
-for answer in assignment.answers[0]:
-    print answer.qid, answer.fields[0]
